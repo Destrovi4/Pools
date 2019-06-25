158e22d03e56dd657d6e7c3956b01083b33ecb87
Destructor <destrovi4@gmail.com>
2019-06-25 13:53:34 +0400

    Examples update

--- src/xyz/destr/example/Example1.java
+++ src/xyz/destr/example/Example1.java
@@ -1,31 +1,55 @@
 package xyz.destr.example;
 
-import java.util.function.Consumer;
+import java.util.concurrent.ArrayBlockingQueue;
 
 import xyz.destr.pool.LocalPool;
+import xyz.destr.pool.Pool;
+import xyz.destr.pool.PoolId;
 
 public class Example1 {
 
-	public static class Foo implements Runnable, Consumer<String> {
-
-		protected String text;
-		
-		@Override
-		public void run() {
-			System.out.println("Foo! " + text);
-			this.text = null;
-			System.exit(0);
+	public static final int COUNT = 100;
+	
+	public static class Foo {
+		public int i;
+		public Foo set(int i) {
+			this.i = i;
+			return this;
 		}
-
-		@Override
-		public void accept(String text) {
-			this.text = text;
-		}
-		
 	}
 	
+	public static final PoolId<Foo> poolId = PoolId.get(Foo.class);
+	
+	public static final ArrayBlockingQueue<Foo> queue = new ArrayBlockingQueue<Foo>(3);
+	
 	public static void main(String[] args) {
-		LocalPool.execute("Hello world", Foo.class);
+		new Thread(Example1::source).start();
+		new Thread(Example1::release).start();
 	}
-
+	
+	public static void source() {
+		Pool<Foo> pool = LocalPool.getPool(poolId);
+		try {
+			for(int i = 0; i < COUNT; i++) {
+				queue.put(pool.get().set(i));
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void release() {
+		Pool<Foo> pool = LocalPool.getPool(poolId);
+		try {
+			for(int i = 0; i < COUNT; i++) {
+				Foo foo = queue.take();
+				System.out.println(foo.i);
+				pool.accept(foo);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
